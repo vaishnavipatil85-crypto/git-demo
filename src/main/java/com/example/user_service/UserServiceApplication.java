4f1eadd066fc790e5f0441c8cefb7ef1f50d24ce
unknown <vaishnavipatil85@gmail.com>
2025-12-03 23:01:35 +0530

    added new

--- src/main/java/com/example/user_service/UserServiceApplication.java
+++ src/main/java/com/example/user_service/UserServiceApplication.java
@@ -12,6 +12,7 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+		System.out.println("new ..........");
 	}
 
 	@Bean

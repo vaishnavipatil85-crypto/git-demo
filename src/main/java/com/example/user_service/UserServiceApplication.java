37f8f613570b7d84b2ae8d7f7643a30753bb0dc8
vaishnavipatil85-crypto <vaishnavipatil85@gmail.com>
2025-12-03 23:48:31 +0530

    Update UserServiceApplication.java by dev2

--- src/main/java/com/example/user_service/UserServiceApplication.java
+++ src/main/java/com/example/user_service/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+////////
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
